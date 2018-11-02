e6da4362a694d5bcfeb595c64473e25d9aba9451
Diego M. G. Vieira <diego.menezzes@gmail.com>
2018-11-02 11:06:33 -0300

    final

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-console.log("Funciona!");
+// baguetteBox.run(".grid-gallery", { animation: "slideIn" });
+
+// $(document).ready(() => {
+//   $("#sidebarCollapse").on("click", function() {
+//     $("#sidebar").toggleClass("active");
+//     $(this).toggleClass("active");
+//   });
+// });
